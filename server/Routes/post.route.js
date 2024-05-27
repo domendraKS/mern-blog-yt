ad5f136f40a010875423d2e014a25c10af6d3714
domendraKS <domendrakumarsahu542@gmail.com>
2024-05-27 23:10:28 +0530

    Create get Posts API routes

--- server/Routes/post.route.js
+++ server/Routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { verifyUser } from "../utils/verifyUser.js";
-import { createPost } from "../Controllers/post.controller.js";
+import { createPost, getAllPosts } from "../Controllers/post.controller.js";
 
 const postRoute = express.Router();
 
 postRoute.post("/create-post", verifyUser, createPost);
+postRoute.get("/getAll-posts", getAllPosts);
 
 export default postRoute;
